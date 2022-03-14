ef49c0314436d8a3a2864b3c989ff68833258d8b
derid149 <85456078+derid149@users.noreply.github.com>
2022-03-14 17:47:51 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+AttributeTargets
+
+    /*
+    get
+    set
+    */
+
+
+    Ab MFK
+
+for (int i = 0; i < 10; i++)
+{
+    if (i == 4)
+    {
+        break;
+    }
+    Console.WriteLine(i);
+}
